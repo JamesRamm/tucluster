f554d80b1c5cb6a745a183e017cbc1111f77ba4c
james ramm <jamessramm@gmail.com>
2017-07-28 16:01:32 +0100

    Ensure mongoenginne installed

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'falcon==1.2.0'
+    'falcon==1.2.0',
+    'mongoengine==0.13.0'
 ]
 
 dependency_links = [
